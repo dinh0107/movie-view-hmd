efc04639ddf25e1498a88184f0be08454d51eb67
dinh0107 <duongvandinh.work@gmail.com>
2025-09-13 21:21:18 +0700

    a

--- src/app/countries/[slug]/page.tsx
+++ src/app/countries/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import MoviesPage from "./CountruyClient";
+export const dynamic = "force-static";
 
+export const revalidate = 3600; 
 export { generateMetadata } from "./metadata";
 
 export default function Page() {
