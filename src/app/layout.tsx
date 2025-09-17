7e95fb2520ef8c94fee611fcc4f078315512b847
dinh0107 <duongvandinh.work@gmail.com>
2025-09-17 14:32:11 +0700

    hiden adss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,7 +83,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <MenuProvider>
           <HeaderLayout />
           {children}
-          <BottomAdBanner />
+          {/* <BottomAdBanner /> */}
           <Analytics />
           <SpeedInsights />
           <FooterLayout />
